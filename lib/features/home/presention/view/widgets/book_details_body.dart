a915d7d27de052cdb717fa9583beffef128f2b69
salma <zweensam@gmail.com>
2025-12-07 18:52:09 +0200

    title done

--- lib/features/home/presention/view/widgets/book_details_body.dart
+++ lib/features/home/presention/view/widgets/book_details_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/Utils/styles.dart';
 import 'package:bookly/features/home/presention/view/widgets/book_list_item.dart';
 import 'package:bookly/features/home/presention/view/widgets/custom_book_details_appbar.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,25 @@ class BookDetailsBody extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: width * .3),
           child: const BookListItem(),
-        )
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          "Harry Potter and Gilbert Fair",
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "Rudyard Kipling",
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+        ),
       ],
     );
   }
